4ac26629dc4c1f333bec43382d4b2cb65a193189
ZAINAB SADAQAT <f2022266101@umt.edu.pk>
2026-05-18 19:51:16 +0000

    datatypes

---
+++ basics/dataty.js
@@ -0,0 +1,7 @@
+"usestrict";
+let name = "Zainab Sadaqat"
+let age = 22
+let isLoggedIn = false
+console.log(typeof name)
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const accountId = 223
+let accountEmail = "zainabsadaqat5@gmail.com"
+let accountPassword = "password123"
+var accountBalance = "1000"
+//accountId = 21
+/* prefer not to use var --block scope and functional scope bcz of */
+accountCity = "Karachi"
+accountEmail = "hello@example.com"
+accountBalance = "2000"
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountBalance, accountCity])
